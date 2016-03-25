8feb97d42724c9e02f284c227aa1f9830e8b6d24
edutav <edutav@gmail.com>
2016-03-25 18:37:04 -0400

    alteração na classe para adequar ao padrão JPA

--- src/main/java/br/com/tcc/model/Disciplina.java
+++ src/main/java/br/com/tcc/model/Disciplina.java
@@ -1,12 +1,15 @@
 package br.com.tcc.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @SuppressWarnings("serial")
@@ -22,6 +25,9 @@ public class Disciplina implements Serializable {
 	@Column(length=30, unique=true,nullable=false)
 	private String disnome;
 	
+	@OneToMany(mappedBy = "disciplinas", fetch = FetchType.EAGER)
+    private List<Ministra> listMinistra;
+	
 	public void setDiscodigo(Integer discodigo) {
 		this.discodigo= discodigo;
 	}
@@ -35,6 +41,32 @@ public class Disciplina implements Serializable {
 	}
 	public String getDisnome() {
 		return this.disnome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((discodigo == null) ? 0 : discodigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		if (discodigo == null) {
+			if (other.discodigo != null)
+				return false;
+		} else if (!discodigo.equals(other.discodigo))
+			return false;
+		return true;
 	}
 
 }
