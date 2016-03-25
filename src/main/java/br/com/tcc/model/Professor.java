09ad49abd86e355bcbfe4313438d663f696e0511
edutav <edutav@gmail.com>
2016-03-25 18:02:58 -0400

    Adicionada classe que representa a tabela professores já utilizando anotações JPA. Adicionado métodos de equals e hashcode.

---
+++ src/main/java/br/com/tcc/model/Professor.java
@@ -0,0 +1,116 @@
+package br.com.tcc.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "professores")
+public class Professor implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(length = 3)
+	private Integer procodigo;
+
+	@Column(length = 6, nullable = false, unique = true)
+	private int promatricula;
+
+	@Column(length = 80, nullable = true)
+	private String pronome;
+
+	@OneToMany(mappedBy = "professores")
+	private List<Ministra> listMinistra;
+
+	@OneToMany(mappedBy = "professores")
+	private List<Telefone> listTelefone;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Habilitacao habilitacao;
+
+	public Integer getProcodigo() {
+		return procodigo;
+	}
+
+	public void setProcodigo(Integer procodigo) {
+		this.procodigo = procodigo;
+	}
+
+	public int getPromatricula() {
+		return promatricula;
+	}
+
+	public void setPromatricula(int promatricula) {
+		this.promatricula = promatricula;
+	}
+
+	public String getPronome() {
+		return pronome;
+	}
+
+	public void setPronome(String pronome) {
+		this.pronome = pronome;
+	}
+
+	public List<Ministra> getListMinistra() {
+		return listMinistra;
+	}
+
+	public void setListMinistra(List<Ministra> listMinistra) {
+		this.listMinistra = listMinistra;
+	}
+
+	public List<Telefone> getListTelefone() {
+		return listTelefone;
+	}
+
+	public void setListTelefone(List<Telefone> listTelefone) {
+		this.listTelefone = listTelefone;
+	}
+
+	public Habilitacao getHabilitacao() {
+		return habilitacao;
+	}
+
+	public void setHabilitacao(Habilitacao habilitacao) {
+		this.habilitacao = habilitacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((procodigo == null) ? 0 : procodigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Professor other = (Professor) obj;
+		if (procodigo == null) {
+			if (other.procodigo != null)
+				return false;
+		} else if (!procodigo.equals(other.procodigo))
+			return false;
+		return true;
+	}
+
+}
