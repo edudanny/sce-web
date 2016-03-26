913ac800e1ad3df4dc8ef1f098d3632eb0e1f183
edutav <edutav@gmail.com>
2016-03-26 10:46:42 -0400

    Adicionado método equals e hashcode

--- src/main/java/br/com/tcc/model/AnoLetivo.java
+++ src/main/java/br/com/tcc/model/AnoLetivo.java
@@ -11,31 +11,57 @@ import javax.persistence.Table;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name="anos")
+@Table(name = "anos")
 public class AnoLetivo implements Serializable {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(length=1)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(length = 1)
 	private Integer anocodigo;
-	
-	@Column(length=2, nullable=false, unique=true)
+
+	@Column(length = 2, nullable = false, unique = true)
 	private String anonome;
-	
-	public Integer getAnocodigo(){
+
+	public Integer getAnocodigo() {
 		return this.anocodigo;
 	}
-	
-	public void setAnocodigo(Integer anocodigo){
+
+	public void setAnocodigo(Integer anocodigo) {
 		this.anocodigo = anocodigo;
 	}
-	
-	public String getAnonome(){
+
+	public String getAnonome() {
 		return this.anonome;
 	}
-	
-	public void setAnonome(String anonome){
+
+	public void setAnonome(String anonome) {
 		this.anonome = anonome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((anocodigo == null) ? 0 : anocodigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnoLetivo other = (AnoLetivo) obj;
+		if (anocodigo == null) {
+			if (other.anocodigo != null)
+				return false;
+		} else if (!anocodigo.equals(other.anocodigo))
+			return false;
+		return true;
 	}
 
 }
