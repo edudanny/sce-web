9d3bf7c713851834eb859306b3403ccd2c71ff86
edutav <edutav@gmail.com>
2016-03-26 11:06:51 -0400

    adicionado atributo que representa o relacionamento entre agenda e secretaria.

--- src/main/java/br/com/tcc/model/Agenda.java
+++ src/main/java/br/com/tcc/model/Agenda.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -31,6 +33,10 @@ public class Agenda implements Serializable{
 	
 	@Column(length=11)
 	private Integer agerescpf;
+	
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Secretaria ageseccodigo;
 	
 	public Integer getAgecodigo(){
 		return this.agecodigo;
@@ -63,4 +69,39 @@ public class Agenda implements Serializable{
 	public void setAgerescpf(Integer agerescpf){
 		this.agerescpf = agerescpf;
 	}
+
+	public Secretaria getAgeseccodigo() {
+		return ageseccodigo;
+	}
+
+	public void setAgeseccodigo(Secretaria ageseccodigo) {
+		this.ageseccodigo = ageseccodigo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((agecodigo == null) ? 0 : agecodigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agenda other = (Agenda) obj;
+		if (agecodigo == null) {
+			if (other.agecodigo != null)
+				return false;
+		} else if (!agecodigo.equals(other.agecodigo))
+			return false;
+		return true;
+	}
+	
 }
